633ab7f61593352690bdf835159817fa849fde48
Андрей Бычков <andrew-by@hotmail.com>
2019-10-07 20:31:10 +0300

    Базовая реализация

---
+++ ByteSplitter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bytes = new byte[] { 0x01, 0x02, 0x03, 0x04, (byte)'\r', (byte)'\n', 0x04, 0x03, 0x02, (byte)'\r', (byte)'\n', 0x02, 0x03, 0x04 };
+            var s = new Splitter(Encoding.ASCII.GetBytes("\r\n"));
+            s.Append(bytes);
+            var chunks = s.PopChunks();
+        }
+    }
+}
---
+++ ByteSplitter/Splitter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteSplitter
+{
+    public class Splitter
+    {
+        private List<byte> _bytes;
+        private byte[] _delimeter;
+
+        public Splitter(byte[] delimiter)
+        {
+            _bytes = new List<byte>();
+            _delimeter = delimiter;
+        }
+
+        public void Append(byte[] bytes)
+        {
+            _bytes.AddRange(bytes);
+        }
+
+        public byte[][] PopChunks()
+        {
+            var parts = new List<byte[]>();
+            var index = 0;
+            for (var i = 0; i < _bytes.Count; ++i)
+            {
+                if (Equals(i))
+                {
+                    parts.Add(_bytes.GetRange(index, i - index).ToArray());
+                    index = i + _delimeter.Length;
+                    i += _delimeter.Length - 1;
+                }
+            }
+            //parts.Add(_bytes.GetRange(index, _bytes.Count - index).ToArray());
+            _bytes.RemoveRange(0, index);
+            return parts.ToArray();
+        }
+
+        private bool Equals(int index)
+        {
+            for (int i = 0; i < _delimeter.Length; ++i)
+                if (index + i >= _bytes.Count || _bytes[index + i] != _delimeter[i])
+                    return false;
+            return true;
+        }
+    }
+}
